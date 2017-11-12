3126bf7be8def3d2927806d04e82015a834a8252
John <johnjmartiniv@gmail.com>
2017-11-12 15:30:51 -0500

    don't track _site, remove smooth scroll

--- _site/assets/jumbotron-image.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var NUM_PHOTOS = 7;
-  var rand = Math.floor((Math.random() * NUM_PHOTOS) + 1);
-  var $jumbotron = $('.jumbo#image-target');
-  if($jumbotron) {
-    $jumbotron.attr('style', "background-image: url(/images/jumbotron/" + rand +".jpg)")
-  }
-});
